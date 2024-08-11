ce9fbb9ee57b83166cea8c8c7818fd06341ada77
AlessioMartella7 <alessio.mart87@gmail.com>
2024-08-11 12:42:10 +0200

    added section "ps4 " layout

--- js/script.js
+++ js/script.js
@@ -7,14 +7,13 @@ gallery.classList.add('row', 'row-cols-2', 'row-cols-sm-3', 'row-cols-xxl-6');
 for(let i = 1; i <= 12; i++) {
     // isoliamo i multipli di 2 e mettiamo all'interno l'img di Crash
     if(i % 2 === 0 ) {
-gallery.innerHTML += `<div class="col"><img class="img-fluid rounded-4" src="img/crash-bandicoot-4-its-about-time-store-art.jpg" alt="">
+gallery.innerHTML += `<div class="col"><img class="img-fluid rounded-4" src="img/crash-bandicoot-4-its-about-time-store-art.jpg" alt="crash"><h4> Crash </h4>
 </div>` ;
-console.log('gallery', gallery)
     }
 
     //mettiamo all'interno dei rimanenti l'img di tony-hawks
     else {
-        gallery.innerHTML += `<div class="col"><img class="img-fluid rounded-4" src="img/tony-hawks-pro-skater-1-2-store-art.jpg" alt="">
+        gallery.innerHTML += `<div class="col"><img class="img-fluid rounded-4" src="img/tony-hawks-pro-skater-1-2-store-art.jpg" alt="tony"><h4> Tony </h4>
         </div>` ;
         console.log('gallery', gallery)
             }
