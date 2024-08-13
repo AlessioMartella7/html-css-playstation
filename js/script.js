7b730c07632ab47c67a9222c656ca336429f061a
AlessioMartella7 <alessio.mart87@gmail.com>
2024-08-13 14:03:32 +0200

    added link to "new-games" thumbnails

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ gallery.classList.add('row', 'row-cols-2', 'row-cols-sm-3', 'row-cols-xxl-6');
 for(let i = 1; i <= 12; i++) {
     // isoliamo i multipli di 2 e mettiamo all'interno l'img di Crash
     if(i % 2 === 0 ) {
-        gallery.innerHTML += `<div class="col gy-4"><img class="img-fluid rounded-4" src="img/crash-bandicoot-4-its-about-time-store-art.jpg" alt="crash"><p class="text-white fs-6 fw-bold mt-2"> Crash Bandicoot 4: It's About Time </p>
+        gallery.innerHTML += `<div class="col gy-4"><a href='#'><img class="img-fluid rounded-4" src="img/crash-bandicoot-4-its-about-time-store-art.jpg" alt="crash"></a><p class="text-white fs-6 fw-bold mt-2"> Crash Bandicoot 4: It's About Time </p>
         </div>` ;
     }
     //mettiamo all'interno dei rimanenti l'img di tony-hawks
     else {
-        gallery.innerHTML += `<div class="col gy-4"><img class="img-fluid rounded-4" src="img/tony-hawks-pro-skater-1-2-store-art.jpg" alt="tony"><p class="text-white fs-6 fw-bold mt-2"> Tony Hawk™ Pro Skater™ 1 + 2 </p>
+        gallery.innerHTML += `<div class="col gy-4"><a href='#'><img class="img-fluid rounded-4" src="img/tony-hawks-pro-skater-1-2-store-art.jpg" alt="tony"></a><p class="text-white fs-6 fw-bold mt-2"> Tony Hawk™ Pro Skater™ 1 + 2 </p>
         </div>` ;
         console.log('gallery', gallery)
     }
